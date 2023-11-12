1d4ae806ec75a3e30529d7b53e79ef65a172e918
David Valencia <davidvalencia0526@gmail.com>
2023-11-12 05:03:04 -0500

    cambio image por thumbnail

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,14 +2,23 @@ import express from "express";
 
 const router = express.Router();
 
-import { createProduct, listProducts, listProductsByCategory, getProduct, updateProduct, deleteProduct, uploadImage, upload } from "../controller/productController.js"
+import {
+  createProduct,
+  listProducts,
+  listProductsByCategory,
+  getProduct,
+  updateProduct,
+  deleteProduct,
+  uploadImage,
+  upload,
+} from "../controller/productController.js";
 
-router.post('/', createProduct);
-router.get('/', listProducts);
-router.get('/category/:categoryId', listProductsByCategory);
-router.get('/:id', getProduct);
-router.put('/:id', upload.single("image"), updateProduct);
-router.delete('/:id', deleteProduct);
+router.post("/", createProduct);
+router.get("/", listProducts);
+router.get("/category/:categoryId", listProductsByCategory);
+router.get("/:id", getProduct);
+router.put("/:id", upload.single("thumbnail"), updateProduct);
+router.delete("/:id", deleteProduct);
 router.post("/upload", upload.single("image"), uploadImage);
 
 export default router;
